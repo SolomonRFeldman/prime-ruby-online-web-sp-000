87f2a048915396d275ea281a7f4d5fa15e5c8ffc
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-19 12:33:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(num)
-  if num == -1 || num == 0 || num == 1
-    return false
-  end
+  # if num == -1 || num == 0 || num == 1
+  #   return false
+  # end
   # if num == 2 || num == -2
   #   return true
   # end
